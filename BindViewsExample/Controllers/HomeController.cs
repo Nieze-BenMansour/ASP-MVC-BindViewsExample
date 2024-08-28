e1786693c91e1def0effd3eb486e85f825e33128
Nieze Ben Mansour <nieze.benmansour@outlook.fr>
2024-08-28 10:04:57 +0100

    Add project files.

---
+++ BindViewsExample/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using BindViewsExample.Models;
+
+namespace BindViewsExample.Controllers;
+
+public class HomeController : Controller
+{
+    public IActionResult Index()
+    {
+        Restaurant restaurant = new Restaurant() { Id = 1, Name = "My Kitchen 1", Address = "New Brunswick, 2657 Webster Street", Speciality = "Hamburgers", Open = true, Review = 4 };
+        return View(restaurant);
+    }
+
+    [Route("Home/Display")]
+    public IActionResult AnotherWayToDisplay()
+    {
+        Restaurant restaurant = new Restaurant() { Id = 2, Name = "My Kitchen 2", Address = "New Brunswick, 4175 Echo Lane Street", Speciality = "Sushi", Open = true, Review = 3 };
+        return View(restaurant);
+    }
+}
---
+++ BindViewsExample/Models/Restaurant.cs
@@ -0,0 +1,16 @@
+﻿namespace BindViewsExample.Models;
+
+public class Restaurant
+{
+    public int Id { get; set; }
+
+    public string Name { get; set; } = string.Empty;
+
+    public string Address { get; set; } = string.Empty;
+
+    public bool Open { get; set; }
+
+    public string Speciality { get; set; } = string.Empty;
+
+    public int Review { get; set; }
+}
